b41432943e813752a00021c125fdda0f330d7c62
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-02 21:37:24 +0200

    Programa08 terminado

---
+++ Programas/Programa07.py
@@ -0,0 +1,12 @@
+# Realizar un programa que lea cuatro valores numéricos e informar su suma y promedio
+
+numero1=int(input("Deme el primer número: "))
+numero2=int(input("Deme el segundo número: "))
+numero3=int(input("Dame el tercer número: "))
+numero4=int(input("Deme el cuarto número: "))
+
+suma=numero1+numero2+numero3+numero4
+promedio=suma/4
+
+print("La suma de todos los números es: ", suma)
+print("El promedio de todos los números es: ", promedio)
