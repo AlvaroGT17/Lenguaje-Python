cc32475838bb8d2f2cb1e20872034ee0f1085f07
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-02 20:01:31 +0200

    Programa03 terminado

---
+++ Programas/Programa03.py
@@ -0,0 +1,14 @@
+# Lo primero es definir las variables y darles un valor, el valor sera introducido por teclado.
+
+numero1=int(input("Ingrese un número: "))
+numero2=int(input("Ingrese el segundo número "))
+
+# Ahora lo que haremos será realizar las operaciones oportunas.
+
+suma=numero1+numero2
+producto=numero1*numero2
+
+# por ultimo daremos el resultado por pantalla.
+
+print("La suma de los dos número da: ", suma)
+print("El Producto de los dos números es: ", producto)
