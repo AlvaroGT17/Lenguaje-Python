3f18ec1f4458087f10b658f306ed9fd576b75b46
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-06 22:13:12 +0200

    Create Programa09.py
    Programa09 terminado

---
+++ Programas/Programa09.py
@@ -0,0 +1,7 @@
+# Ingresar el sueldo de una persona, si supera los 3000 dolares mostrar un mensaje en pantalla indicando que debe abonar impuestos.
+
+sueldo=int(input("Ingrese el sueldo del operario: "))
+if sueldo > 3000:
+  input("Hacienda a este lo cruje")
+else:
+  input("Este sera pobre toda su vida")
