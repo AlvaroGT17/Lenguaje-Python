1d62da5db024963f4342c0fc511d39494efa8446
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-07 13:47:55 +0200

    Programa10 terminado

--- Programas/Programa10.py
+++ Programas/Programa10.py
@@ -7,3 +7,15 @@ if numero1>numero2:
   print("El número ", numero1, " es mayor que ", numero2)
 else:
   print("El número ", numero2, " es mayor que ", numero1)
+
+# Otra forma de hacer este ejercicio es de la siguiente manera: 
+
+# num1=int(input("Ingrese primer valor:"))
+# num2=int(input("ingrese segundo valor:"))
+# print("El valor mayor es")
+# if num1>num2:
+#    print(num1)
+# else:
+#    print(num2)
+
+# De esta manera queda mas claro ya que solo imprimes un solo mensaje y lo único que hará el programa es comparar los números.
