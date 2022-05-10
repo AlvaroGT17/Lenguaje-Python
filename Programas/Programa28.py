a80cd31222786f05310f03d7d31e68e578c7ab9a
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-10 10:16:58 +0200

    Programa28 terminado

---
+++ Programas/Programa28.py
@@ -0,0 +1,36 @@
+# Realizar un programa que imprima en pantalla los números del 1 al 100.
+
+x=1
+while x<=100:
+  print(x)
+  x=x+1
+input("Pulsa una tecla para continuar.")
+
+# 1 - Imprimir los números del 1 al 500.
+# 2 - Imprimir los números del 50 al 100.
+# 3 - Imprimir los números del -50 al 0.
+# 4 - Imprimir los números del 2 al 100 pero de 2 en 2 (2,4,6,8 ....100).
+
+x=1
+while x<=500:
+  print(x)
+  x=x+1
+input("Pulsa una tecla para continuar.")
+
+x=50
+while x<=100:
+  print(x)
+  x=x+1
+input("Pulsa una tecla para continuar.")
+
+x=-50
+while x<=0:
+  print(x)
+  x=x+1
+input("Pulsa una tecla para continuar.")
+
+x=2
+while x<=100:
+  print(x)
+  x=x+2
+input("Pulsa una tecla para continuar.")
