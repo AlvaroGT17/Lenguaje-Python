b60fa688a5698737a9a1c6d1c97f912c92f347d9
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-13 21:49:00 +0200

    Programa36 terminado

---
+++ Programas/Programa36.py
@@ -0,0 +1,10 @@
+# Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en pantalla 8 - 16 - 24, etc
+
+x=1
+contador=500
+multipo=0
+
+while x<=500:
+  multipo=8*x
+  print(multipo)
+  x=x+1
