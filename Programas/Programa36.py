94faa0a4a643a1ce7317301474282f396b687670
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-13 22:02:53 +0200

    corrección del ejercicio

--- Programas/Programa36.py
+++ Programas/Programa36.py
@@ -1,10 +1,8 @@
 # Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en pantalla 8 - 16 - 24, etc
 
-x=1
 contador=500
 multipo=0
 
-while x<=500:
-  multipo=8*x
+while multipo<=500:
   print(multipo)
-  x=x+1
+  multipo=multipo+8
