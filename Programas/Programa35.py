568c2fd8d1dcdd82212d072d708a5501c0067a50
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-13 21:43:20 +0200

    Programa35 terminado

---
+++ Programas/Programa35.py
@@ -0,0 +1,10 @@
+# Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44, etc. (No se ingresan valores por teclado)
+
+x=1
+contador=25
+numero=11
+
+while x<contador:
+  print(numero)
+  numero=numero+11
+  x=x+1
