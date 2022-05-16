c11e2f07d34f36709b738aa81c236f485bf93924
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-16 11:54:05 +0200

    Programa59 terminado

---
+++ Programas/Programa59.py
@@ -0,0 +1,12 @@
+# Realizar la carga de dos nombres por teclado. Mostrar cual de los dos es mayor alfabéticamente o si son iguales.
+
+nombre1=input("Ingrese el nombre del sujeto 1: ")
+nombre2=input("Ingrese el nombre del sujeto 2: ")
+
+if nombre1>nombre2:
+  print("El nombre del sujeto 1 es mayor.")
+elif nombre1==nombre2:
+
+  print("El nombre del sujeto 1 y el del sujeto 2 son iguales.")
+else:
+  print("El nombre del sujeto 2 es mayor.")
