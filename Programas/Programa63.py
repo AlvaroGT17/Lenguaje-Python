c02b2c82d70924d39429958dcce00abd34a77944
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-16 13:55:25 +0200

    Programa63 terminado

--- Programas/Programa63.py
+++ Programas/Programa63.py
@@ -2,7 +2,7 @@
 
 nombre=input("Ingrese un nombre minúsculas: ")
 
-if nombre[0]=="a" or "e" or "i" or "o" or "u":
+if nombre[0] == "a" or nombre[0] == "e" or nombre[0] == "i" or nombre[0] == "o" or nombre[0] == "u":
   print("El nombre introducido empieza por vocal")
 else:
   print("El nombre introducido empieza por consonante")
