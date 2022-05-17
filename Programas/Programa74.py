58f53f8665c96714cece66515e2d99155905bdfb
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-17 17:13:56 +0200

    Programa74 empezado

---
+++ Programas/Programa74.py
@@ -0,0 +1,8 @@
+# Definir una lista que almacene por asignación los nombres de 5 personas. Contar cuantos de esos nombres tienen 5 o más caracteres.
+
+nombres=["Jesus", "Alvaro", "Aaron", "Carlos", "Raúl"]
+
+x=0
+
+while x<len(nombres):
+  
