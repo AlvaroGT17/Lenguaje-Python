33522b92c465e04bc858076907184003372f947f
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-17 13:19:54 +0200

    Programa71 terminado

---
+++ Programas/Programa71.py
@@ -0,0 +1,9 @@
+# Definir una lista por asignación que almacene en la primer componente el nombre de un alumno y en las dos siguientes sus notas.
+# Imprimir luego el nombre y el promedio de las dos notas.
+
+alumno=["Jesus", 9.75, 10]
+promedio=0
+
+print("Nombre del alumno: ", alumno[0])
+promedio=(alumno[1]+alumno[2])/2
+print("Su promedio es de: ", promedio)
