46bedc7e7d76ff242de0d0b25785ecf03090895a
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-17 11:33:08 +0200

    Programa65 terminado

---
+++ Programas/Programa65.py
@@ -0,0 +1,11 @@
+# Inicializar un string con la cadena "mAriA" luego llamar a sus métodos upper(), lower() y capitalize(), guardar los datos retornados en otros
+# string y mostrarlos por pantalla.
+
+nombreorigen="mAriA"
+nombreminisculas=nombreorigen.lower()
+nombremayusculas=nombreorigen.upper()
+nombrecapital=nombreorigen.capitalize()
+
+print("Este es el nombre con la primera letra en mayúsculas y el resto en minúsculas ", nombrecapital)
+print("Este es el nombre con todas las letras en minúsculas", nombreminisculas)
+print("Este es el nombre con todas las letras en mayúsculas", nombremayusculas)
