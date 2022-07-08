8a56676048631fa9032fa998c8661f23044eddd7
Alvaro Agt. Gomez Tejada <103853558+AlvaroGT17@users.noreply.github.com>
2022-07-08 09:38:25 +0200

    Programa141 terminado

---
+++ Programas/Programa141.py
@@ -0,0 +1,7 @@
+# Cargar una lista de 10 enteros, luego mostrarlos por pantalla a cada elemento separados por una coma.
+
+for x in range(10):
+  if x==9:
+    print(x)
+  else:
+    print(x, end=",")
