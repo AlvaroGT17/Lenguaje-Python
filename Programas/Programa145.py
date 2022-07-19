d75ff835776b766df8021ed80062f05a8c82911d
Alvaro Agt. Gomez Tejada <103853558+AlvaroGT17@users.noreply.github.com>
2022-07-19 21:30:37 +0200

    Programa145 terminado

---
+++ Programas/Programa145.py
@@ -0,0 +1,14 @@
+# Definir varias tuplas e imprimir sus elementos.
+
+from sqlite3 import ProgrammingError
+
+
+primeratupla=(1,3,4 ,3)
+fecha=(19,"Julio",2022)
+punto=(10, 2)
+persona=("Alvaro","Gómez",39)
+
+print (primeratupla)
+print (fecha)
+print (punto)
+print (persona)
