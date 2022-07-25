1ba66aa85f5c210d8e3ee7d7daed5976c6567907
Alvaro Agt. Gomez Tejada <103853558+AlvaroGT17@users.noreply.github.com>
2022-07-25 23:54:48 +0200

    Programa151 modificado y terminado

--- Programas/Programa151.py
+++ Programas/Programa151.py
@@ -1,4 +1,4 @@
-# Almacenar en una lista de 5 elementos tuplas que guarden el nombre de un pais y la cantidad de habitantes.
+# Almacenar en una lista de 5 elementos tuplas que guarden el nombre de un país y la cantidad de habitantes.
 # Definir tres funciones, en la primera cargar la lista, en la segunda imprimirla y en la tercera mostrar el nombre del país con mayor cantidad
 # de habitantes.
 
